0471fc4a65b5ee6f3f2150562342170ab6758bcb
juanstar <grattpotter@gmail.com>
2021-11-26 20:37:31 -0600

    Version 1.1

---
+++ Documents/NetBeansProjects/ArbolBinario/src/arbolbinario/IBinaryTree.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arbolbinario;
+
+/**
+ *
+ * @author j_sta
+ */
+public interface IBinaryTree {
+    public void insert (Nodo nodo, int value);
+    public void traverseInOrder(Nodo nodo);
+    
+    
+}
